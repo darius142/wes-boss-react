93f021b8215f33d34473d1519c851d0aa716f003
Volodymyr Padovsky <vld06@ukr.net>
2018-01-11 23:36:13 +0200

    Router Added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Browserouter, Match, Miss} from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
 
 import StorePicker from './components/StorePicker';
+
+const Root = () => {
+  return (
+    <Browserouter>
+      <div>
+        <Match exacly pattern="/" component={StorePicker} />
+        <Match  pattern="/store/:storeId" component={App} />
+        <Miss component={notFound} />
+      </div>
+    </Browserouter>
+  )
+}
 
 ReactDOM.render(
   <App />,
